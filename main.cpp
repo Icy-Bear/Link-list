e661a6b141fdb518e59faaf33d406ff4d8c9e6f3
Icy Bear <prakhar.aka.nb@gmail.com>
2024-04-14 00:51:47 +0530

    Class Added

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include<iostream>
 
+// class Node
+// {
+// public:
+//     int val;
+//     void* next = NULL;
+
+//     Node(int val , void* next) : val(val) , next(next) {}
+
+//     void print(Node n)
+//     {
+//         while (n.next != NULL ){
+//             std::cout << n.val << std::endl;
+//         }
+//     }
+
+// };
+
 int main()
 {
-    std::cout << "Hello" << std::endl;
+    std::cout << "Hello World" << std::endl;
     return 0;
 }
